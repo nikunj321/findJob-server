29f8fd3cfa671689e314e1cb307c08d32f2d8f13
nikunj321 <nikunjc43@gmail.com>
2021-04-15 21:02:37 +0530

    typo in contains

--- src/resolvers/queryQuestion.js
+++ src/resolvers/queryQuestion.js
@@ -1,22 +1,27 @@
 module.exports = {
     Query: {
         searchQuestion: async (_, { keyword, companyId }, { prisma }) => {
-            const availableQuestions = await prisma.question.findMany({
-                where: {
-                    AND: [
-                        {
-                            title: {
-                                constains: keyword
+            try {
+                const availableQuestions = await prisma.question.findMany({
+                    where: {
+                        AND: [
+                            {
+                                title: {
+                                    contains: keyword
+                                }
+                            },
+                            {
+                                companyId
                             }
-                        },
-                        {
-                            companyId
-                        }
-                    ]
-                }
-            });
+                        ]
+                    }
 
-            return availableQuestions;
+                });
+
+                return availableQuestions;
+            } catch (err) {
+                throw new Error('something went wrong while fetching Question');
+            }
         }
     }
 }
